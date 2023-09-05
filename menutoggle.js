404297c28339bf9f2a4224aac353bc4a25e770d5
João Victor M. Da Costa <108638735+JoaoVictorMCosta@users.noreply.github.com>
2023-09-05 10:30:00 -0300

    Update menutoggle.js

--- menutoggle.js
+++ menutoggle.js
@@ -22,28 +22,41 @@ document.getElementById("logo").addEventListener("click", function (event) {
     redirectToHomePage();
 });
 
+function opengithub(){
+  window.open('https://github.com/JoaoVictorMCosta','_blank');
+  
+}
+document.getElementById('openGitHub').addEventListener('click',opengithub);
 
-// Mostra o iframe do github
-document.getElementById('openGitHub').addEventListener('click', function() {
-  var socialIframe = document.getElementById('socialIframe');
-  if (socialIframe.style.display === 'none') {
-      socialIframe.style.display = 'block'; // Mostrar o iframe
-      socialIframe.src="http://www.github.com/JoaoVictorMCosta";
-  } else {
-      socialIframe.style.display = 'none'; // Ocultar o iframe se já estiver visível
-      socialIframe.src=''; //Limpa o src e para a pagina de ser exibida
-  }
-});
+function openlinkedin(){
+  window.open('https://www.linkedin.com/in/joaovictormdacosta/','_blank');
+  
+}
+document.getElementById('openLinkedIn').addEventListener('click',openlinkedin);
+
+
+//iframe não funciona em sites de terceiros!!!!!!
+// // Mostra o iframe do github
+// document.getElementById('openGitHub').addEventListener('click', function() {
+//   var socialIframe = document.getElementById('socialIframe');
+//   if (socialIframe.style.display === 'none') {
+//       socialIframe.style.display = 'block'; // Mostrar o iframe
+//       socialIframe.src="http://www.github.com/JoaoVictorMCosta";
+//   } else {
+//       socialIframe.style.display = 'none'; // Ocultar o iframe se já estiver visível
+//       socialIframe.src=''; //Limpa o src e para a pagina de ser exibida
+//   }
+// });
   
 
-// Mostra o iframe do linkedin
-document.getElementById('openLinkedIn').addEventListener('click', function() {
-  var socialIframe = document.getElementById('socialIframe');
-  if (socialIframe.style.display === 'none') {
-      socialIframe.style.display = 'block'; // Mostrar o iframe
-      socialIframe.src="http://www.linkedin.com/in/joaovictormdacosta/";
-  } else {
-      socialIframe.style.display = 'none'; // Ocultar o iframe se já estiver visível
-      socialIframe.src=''; //Limpa o src e para a pagina de ser exibida
-  }
-});
+// // Mostra o iframe do linkedin
+// document.getElementById('openLinkedIn').addEventListener('click', function() {
+//   var socialIframe = document.getElementById('socialIframe');
+//   if (socialIframe.style.display === 'none') {
+//       socialIframe.style.display = 'block'; // Mostrar o iframe
+//       socialIframe.src="http://www.linkedin.com/in/joaovictormdacosta/";
+//   } else {
+//       socialIframe.style.display = 'none'; // Ocultar o iframe se já estiver visível
+//       socialIframe.src=''; //Limpa o src e para a pagina de ser exibida
+//   }
+// });
